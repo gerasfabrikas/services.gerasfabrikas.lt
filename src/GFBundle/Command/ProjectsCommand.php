d657aacca19b4e0830064f1bd7bc4933e014dc5e
Mindaugas Liubinas <mindaugas.l@estina.lt>
2016-04-02 09:12:47 +0300

    Save projects

--- src/GFBundle/Command/ProjectsCommand.php
+++ src/GFBundle/Command/ProjectsCommand.php
@@ -5,10 +5,12 @@ namespace GFBundle\Command;
 require_once dirname(dirname(__FILE__)) . '/lib/libphutil/src/__phutil_library_init__.php';
 
 use GFBundle\Service\ApiClient;
+use GFBundle\Entity\Project;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\Bundle\DoctrineBundle\Registry;
 
 class ProjectsCommand extends ContainerAwareCommand
 {
@@ -21,11 +23,47 @@ class ProjectsCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        //$output->writeln('Command result.');
+        //
         /** @var ApiClient $apiClient */
         $apiClient = $this->getContainer()->get('gf.api_client');
+        $projects = $apiClient->getOpenProjects();
 
-        print_r($apiClient->getOpenProjects());
+        $em = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository('GFBundle:Project');
+        $count = 0;
+
+        foreach ($projects['data'] as $projectData) {
+
+            if ($repository->findOneBy(['phid' => $projectData['phid']])) {
+                continue;
+            }
+
+            $project = new Project();
+            $project->setPhid($projectData['phid']);
+            $project->setName($projectData['name']);
+
+            $em->persist($project);
+            $count++;
+        }
+
+        $em->flush();
+        $output->writeln(sprintf('%d saved.', $count));
+    }
+
+    /**
+     * Shortcut to return the Doctrine Registry service.
+     *
+     * @return Registry
+     *
+     * @throws \LogicException If DoctrineBundle is not available
+     */
+    protected function getDoctrine()
+    {
+        if (!$this->getContainer()->has('doctrine')) {
+            throw new \LogicException('The DoctrineBundle is not registered in your application.');
+        }
+
+        return $this->getContainer()->get('doctrine');
     }
 
 }
