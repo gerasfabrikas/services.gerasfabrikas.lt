32abf071e59e00fa39df79ea77d857f6d1d5c7ec
Mindaugas Liubinas <mindaugas.l@estina.lt>
2016-04-08 23:13:44 +0300

    User stats by category and by project

--- src/GFBundle/Command/StatsCommand.php
+++ src/GFBundle/Command/StatsCommand.php
@@ -23,31 +23,61 @@ class StatsCommand extends ApiCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->conn = $this->getDoctrine()->getConnection();
-        $this->clearData();
 
+        $output->writeln('User stats by category...');
+        $this->statsByCategory();
+        $output->writeln('Done');
+
+        $output->writeln('User stats by project...');
+        $this->statsByProject();
+        $output->writeln('Done');
+    }
+
+    /**
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    private function statsByCategory()
+    {
+        $this->conn->query('TRUNCATE `user_stats_by_category`');
         $query =
-            'INSERT INTO `stats` (`name`, company, hours, category)
-                    SELECT u.realname,
-                           u.company,
+            'INSERT INTO `user_stats_by_category` (userId, hours, categoryId)
+                    SELECT u.id,
                            SUM(t.hours) AS hours,
-                           tc.name
+                           tc.id
                     FROM `task` t
                     INNER JOIN `user` u
                         ON u.phid = t.owner
                     INNER JOIN task_category tc
                         ON tc.category = t.category
                     WHERE t.`status` = "resolved"
-                    GROUP BY u.realname,
-                             tc.name';
+                    GROUP BY u.id,
+                             tc.id';
 
         $this->conn->query($query);
-        $output->writeln('Done');
+
     }
 
-    private function clearData()
+    /**
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    private function statsByProject()
     {
-        $this->conn->query('TRUNCATE `stats`');
+        $this->conn->query('TRUNCATE `user_stats_by_project`');
+        $query =
+            'INSERT INTO `user_stats_by_project` (userId, hours, projectId)
+                    SELECT u.id,
+                           SUM(t.hours) AS hours,
+                           p.id
+                    FROM `task` t
+                    INNER JOIN `user` u
+                        ON u.phid = t.owner
+                    INNER JOIN project p
+                        ON p.phid = t.project
+                    WHERE t.`status` = "resolved"
+                    GROUP BY u.id,
+                             p.id';
 
+        $this->conn->query($query);
     }
 
 
