4272bab56a64c93e1329c3e5cbad435d95cdaefd
Mindaugas Liubinas <mindaugas.l@estina.lt>
2016-04-08 22:56:55 +0300

    Stats init

--- src/GFBundle/Command/StatsCommand.php
+++ src/GFBundle/Command/StatsCommand.php
@@ -5,8 +5,14 @@ namespace GFBundle\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use Doctrine\DBAL\Connection;
+
 class StatsCommand extends ApiCommand
 {
+    /** @var Connection */
+    private $conn;
+
+
     protected function configure()
     {
         $this
@@ -16,21 +22,34 @@ class StatsCommand extends ApiCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->conn = $this->getDoctrine()->getConnection();
+        $this->clearData();
 
-        $sql = 'SELECT u.realname,
-                       SUM(t.hours) AS hours,
-                       tc.name
-                FROM `task` t
-                       INNER JOIN `user` u
-                               ON u.phid = t.owner
-                       INNER JOIN task_category tc
-                               ON tc.category = t.category
-                WHERE  t.`status` = \'resolved\'
-                GROUP  BY u.realname,
-                          tc.name';
+        $query =
+            'INSERT INTO `stats` (`name`, company, hours, category)
+                    SELECT u.realname,
+                           u.company,
+                           SUM(t.hours) AS hours,
+                           tc.name
+                    FROM `task` t
+                    INNER JOIN `user` u
+                        ON u.phid = t.owner
+                    INNER JOIN task_category tc
+                        ON tc.category = t.category
+                    WHERE t.`status` = "resolved"
+                    GROUP BY u.realname,
+                             tc.name';
 
+        $this->conn->query($query);
         $output->writeln('Done');
+    }
+
+    private function clearData()
+    {
+        $this->conn->query('TRUNCATE `stats`');
 
     }
+
+
 
 }
